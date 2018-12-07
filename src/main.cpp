f41838016d5bcd8941e611e2130318590775cec8
Howstar <506168507@qq.com>
2018-12-07 10:53:14 +0800

    add dynamic parameter

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,9 @@
 #include <pcl/common/time.h>
 #include <fstream>
 
+#include <dynamic_reconfigure/server.h>
+#include <rfans_merge/merge_Config.h>
+
 using namespace global_utility;
 using namespace std;
 
@@ -123,8 +126,8 @@ void getParam(ros::NodeHandle nh)
 
 void savePoints(cloudPtr cloud_16l, cloudPtr cloud_32l, int frame_num)
 {
-    char* cwd = NULL;
-    cwd = getcwd(NULL,0);
+    char *cwd = NULL;
+    cwd = getcwd(NULL, 0);
 
     const char *path16 = "bkth16l";
     int mode_all = S_IRWXU | S_IRWXG | S_IRWXO;
@@ -143,7 +146,7 @@ void savePoints(cloudPtr cloud_16l, cloudPtr cloud_32l, int frame_num)
             ss1 << str << "/" << str << "_" << idx << ".txt";
             std::string str1;
             ss1 >> str1;
-            ofstream file(str1,ios::app);
+            ofstream file(str1, ios::app);
             file << cloud_16l->points[i].x << " "
                  << cloud_16l->points[i].y << " "
                  << cloud_16l->points[i].z << endl;
@@ -165,7 +168,7 @@ void savePoints(cloudPtr cloud_16l, cloudPtr cloud_32l, int frame_num)
             ss1 << str << "/" << str << "_" << idx << ".txt";
             std::string str1;
             ss1 >> str1;
-            ofstream file(str1,ios::app);
+            ofstream file(str1, ios::app);
             file << cloud_32l->points[i].x << " "
                  << cloud_32l->points[i].y << " "
                  << cloud_32l->points[i].z << endl;
@@ -176,10 +179,39 @@ void savePoints(cloudPtr cloud_16l, cloudPtr cloud_32l, int frame_num)
     free(cwd);
 }
 
+void callback(rfans_merge::merge_Config &config, uint32_t level)
+{
+    ROS_INFO("Reconfigure Request: %d %d %d %d %d %d",
+             config.x_16,
+             config.y_16,
+             config.z_16,
+             config.pitch_16,
+             config.roll_16,
+             config.yaw_16);
+
+    g_LiDAR_pos16[0] = config.x_16;
+    g_LiDAR_pos16[1] = config.y_16;
+    g_LiDAR_pos16[2] = config.z_16;
+    g_LiDAR_pos16[3] = config.pitch_16;
+    g_LiDAR_pos16[4] = config.roll_16;
+    g_LiDAR_pos16[5] = config.yaw_16;
+
+    g_LiDAR_pos32[0] = config.x_32;
+    g_LiDAR_pos32[1] = config.y_32;
+    g_LiDAR_pos32[2] = config.z_32;
+    g_LiDAR_pos32[3] = config.pitch_32;
+    g_LiDAR_pos32[4] = config.roll_32;
+    g_LiDAR_pos32[5] = config.yaw_32;
+}
+
 int main(int argc, char **argv)
 {
     // Initialize ROS
     ros::init(argc, argv, "rfans_merge_node");
+    dynamic_reconfigure::Server<rfans_merge::merge_Config> server;
+    dynamic_reconfigure::Server<rfans_merge::merge_Config>::CallbackType fun_cb;
+    fun_cb = boost::bind(&callback, _1, _2);
+    server.setCallback(fun_cb);
     ros::NodeHandle nh;
     ros::NodeHandle nh_private("~");
 
@@ -212,16 +244,31 @@ int main(int argc, char **argv)
             cloud_32l.swap(g_cloud_32l);
             cloud_mutex32.unlock();
         }
-        // if (cloud_16l && cloud_32l)
-        if (cloud_32l)
+        if (cloud_16l && cloud_32l)
+        // if (cloud_32l)
         {
             // frame_num++;
             // savePoints(cloud_16l, cloud_32l, frame_num);
 
-            getParam(nh_private);
+            // getParam(nh_private);
 
-            ros::Time t1=ros::Time::now();
-            // RfansMerge rfans_merge(cloud_16l, cloud_32l);
+            time_t gpsTime16 = cloud_16l->header.stamp & 0xffffffffl;
+            time_t gpsTime32 = cloud_32l->header.stamp & 0xffffffffl;
+
+            auto diff_time = abs(gpsTime16 - gpsTime32);
+            if (diff_time > 1e6)
+            {
+                double dtime = diff_time * 1.0 / 1e6;
+                char msg_str[100];
+                sprintf(msg_str,
+                        "time is not synchronous, the differance of time between two cloud is %f s.",
+                        dtime);
+                ROS_WARN(msg_str);
+                continue;
+            }
+
+            ros::Time t1 = ros::Time::now();
+            RfansMerge rfans_merge(cloud_16l, cloud_32l);
             // int dtime = abs((int)(cloud_16l->header.stamp - cloud_32l->header.stamp));
             // if (dtime)
             // {
@@ -230,21 +277,21 @@ int main(int argc, char **argv)
             // auto euler_angles = rotation_matrix.eulerAngles ( 2,1,0 );
             // std::cout<<euler_angles<<std::endl;
             // }
-            // rfans_merge.merge();
-            // cout<<"merge time is "<<ros::Time::now()-t1<<endl;
+            rfans_merge.merge();
 
-            // CloudPublisher::all_publish(rfans_merge.trans_cloud_16_, rfans_merge.trans_cloud_32_);
-
+            CloudPublisher::all_publish(rfans_merge.trans_cloud_16_, rfans_merge.trans_cloud_32_);
 
             // Segmentation segmentation(cloud_32l);
             // segmentation.segmentAllPlanes();
 
             // CloudPublisher::all_publish(segmentation.getSegmentPoints(),cloud_32l);
 
-            PlaneExtractionBySeed planeExtraction(cloud_32l,32);
-            planeExtraction.segmentAllPlanes();
+            // PlaneExtractionBySeed planeExtraction(cloud_32l,32);
+            // planeExtraction.segmentAllPlanes();
 
-            CloudPublisher::all_publish(planeExtraction.getPlaneCloud(),cloud_32l);
+            // CloudPublisher::all_publish(planeExtraction.getPlaneCloud(),cloud_32l);
+
+            cout << "total time is " << ros::Time::now() - t1 << endl;
         }
         ros::spinOnce();
         loop_rate.sleep();
